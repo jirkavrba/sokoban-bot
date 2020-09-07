75223737d3e1e83bffdefbb2910f5f98d4f999e5
Jiří Vrba <jiri@vrba.dev>
2020-09-07 23:48:36 +0200

    Location addition (implemented for both Location and Direction)

--- src/main/scala/dev/vrba/sokobanbot/game/util/Location.scala
+++ src/main/scala/dev/vrba/sokobanbot/game/util/Location.scala
@@ -1,3 +1,9 @@
 package dev.vrba.sokobanbot.game.util
 
-case class Location(x: Int, y: Int)
+case class Location(x: Int, y: Int) {
+  def + (location: Location): Location =
+    Location(x + location.x, y + location.y)
+
+  def + (direction: Direction): Location =
+    Location(x + direction.x, y + direction.y)
+}
