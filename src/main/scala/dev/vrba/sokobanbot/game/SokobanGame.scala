2997f52391561b7486dddd9073dc9df995d19cd3
Jiří Vrba <jiri@vrba.dev>
2020-09-08 00:46:59 +0200

    Implemented basic level mechanics like move application

--- src/main/scala/dev/vrba/sokobanbot/game/SokobanGame.scala
+++ src/main/scala/dev/vrba/sokobanbot/game/SokobanGame.scala
@@ -1,10 +1,40 @@
 package dev.vrba.sokobanbot.game
 
-import dev.vrba.sokobanbot.{GameState, NotInGame}
-import dev.vrba.sokobanbot.game.level.Level
+import dev.vrba.sokobanbot._
+import dev.vrba.sokobanbot.game.level.{Level, Wall}
+import dev.vrba.sokobanbot.game.util.{Direction, Location}
 
-case class SokobanGame (
+case class SokobanGame(
   state: GameState = NotInGame,
   level: Option[Level] = None,
   moves: Int = 0
-)
+) {
+
+  def applyMove(direction: Direction): SokobanGame = {
+    // Only handle games that are actively in play
+    if (!state.isInstanceOf[Playing.type]) return this
+
+    // Due to state management, this can be safely assured
+    assert(this.level.isDefined)
+
+    val level = this.level.get
+    val target = level.player + direction
+
+    if (level.isValidPlayerPosition(target) &&
+        level.canMoveBox(target, direction)) {
+
+      val updated = level.moveBox(target, direction)
+                         .movePlayer(direction)
+
+      if (updated.isSolved) {
+        return SokobanGame(Won, Some(updated), moves)
+      }
+
+      return SokobanGame(Playing, Some(updated), moves + 1)
+    }
+
+    this
+  }
+
+  private def containsBox(level: Level, location: Location): Boolean = level.boxes.contains(location)
+}
--- src/main/scala/dev/vrba/sokobanbot/game/level/Level.scala
+++ src/main/scala/dev/vrba/sokobanbot/game/level/Level.scala
@@ -1,17 +1,54 @@
 package dev.vrba.sokobanbot.game.level
 
-import dev.vrba.sokobanbot.game.util.Location
+import dev.vrba.sokobanbot.game.util.{Direction, Location}
 
 case class Level(
   tiles: Array[Array[Tile]],
   boxes: Array[Location],
   player: Location
-)
+) {
+  def inBounds(location: Location): Boolean =
+    location.x >= 0 &&
+    location.y >= 0 &&
+    location.x < tiles.length &&
+    // Needs to be handled in this manner,
+    // as if the level is empty (shouldn't be tho) it would rise OutOfBoundsException
+    location.y < tiles.headOption.getOrElse(Array()).length
+
+  def tile(location: Location): Tile =
+    if (inBounds(location)) tiles(location.x)(location.y)
+    else Tile(Location(0, 0), Wall) // Default tile
+
+  def moveBox(targetTile: Location, direction: Direction): Level =
+    this.copy(boxes = boxes.map( box =>
+      if (box == targetTile) targetTile + direction
+      else box
+    ))
+
+  def movePlayer(direction: Direction): Level =
+    this.copy(player = player + direction)
+
+  def isValidPlayerPosition(location: Location): Boolean =
+    this.inBounds(location) &&
+    this.tile(location).material != Wall
+
+  def canMoveBox(from: Location, direction: Direction): Boolean =
+    // Either there is no box on the target tile
+    !boxes.contains(from) ||
+    // Or the box can be pushed in the given direction
+    (tile(from + direction).material != Wall && !boxes.contains(from + direction)) // Two stacked boxes cannot be pushed
+
+  def isSolved: Boolean =
+    this.tiles
+      .flatten
+      .filter(_.material == Target)
+      .forall(tile => boxes.contains(tile.location == _))
+}
 
 object Level {
   // Parse level from a string
   def fromString(source: String): Option[Level] = {
-    // TODO: Implement level parsing from string
+    // TODO: Implement level parsing from string (move to a separated module?)
     None
   }
 }
