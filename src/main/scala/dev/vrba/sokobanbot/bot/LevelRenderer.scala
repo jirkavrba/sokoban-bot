b3ba49fcca13263a441cf4696d088072ff662771
Jiří Vrba <jiri@vrba.dev>
2020-09-08 13:35:41 +0200

    Enhanced level rendering

--- src/main/scala/dev/vrba/sokobanbot/bot/LevelRenderer.scala
+++ src/main/scala/dev/vrba/sokobanbot/bot/LevelRenderer.scala
@@ -3,16 +3,28 @@ package dev.vrba.sokobanbot.bot
 import dev.vrba.sokobanbot.game.level.{Air, Level, Target, Wall}
 
 object LevelRenderer {
+  private val emojis = Map(
+    "wall" -> "\uD83D\uDFEB",
+    "player" -> "\uD83D\uDC77",
+    "box" -> "\uD83D\uDCE6",
+    "air" -> "⬛",
+    "finished_target" -> "\uD83C\uDF1F",
+    "target" -> "⭐"
+  )
+
   def render(level: Level): String = {
     level.tiles
       .map(row => row.map(
         tile => {
-          if (level.player == tile.location) "\uD83D\uDE0A"
-          else if (level.boxes.contains(tile.location)) "\uD83D\uDCE6"
-          else tile.material match {
-            case Wall   => "\uD83D\uDFEA"
-            case Air    => "⬛"
-            case Target => "\uD83C\uDF1F"
+          tile.material match {
+            case Wall   => emojis("wall")
+
+            case Air    => if (level.player == tile.location) emojis("player")
+                           else if (level.boxes.contains(tile.location)) emojis("box")
+                           else emojis("air")
+
+            case Target => if (level.boxes.contains(tile.location)) emojis("finished_target")
+                           else emojis("target")
           }
         }
       ).mkString
